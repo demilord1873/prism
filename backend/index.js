1fa3362f73218f3f81be04cf33a8b7b5f6fe6013
Luis Dem <therealluisdem@gmail.com>
2025-11-08 12:12:03 +0100

    Third Commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4001 ;
+const MONGO_URL = process.env.MONGO_URL;
 
+mongoose.connect(MONGO_URL)
+.then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.error("MongoDB Connection Error: ", error))
 
 app.listen(port, () => {
     console.log(` Server is running on port ${port} `);
