89a381969f664608b96836ef5fea63d528253ccf
Luis Dem <therealluisdem@gmail.com>
2025-11-15 11:31:25 +0100

    Fifth commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
 
+import userRoutes from './routes/user.routes'
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4001 ;
@@ -10,6 +12,8 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL)
 .then(() => console.log("Connected to MongoDB"))
 .catch((error) => console.error("MongoDB Connection Error: ", error))
+
+app.use("/api/v1/user", userRoutes);
 
 app.listen(port, () => {
     console.log(` Server is running on port ${port} `);
